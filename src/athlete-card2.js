ac7b6d070f7b118a17c19c57e07b9a990cf9a8b0
zach-carson <122495829+zach-carson@users.noreply.github.com>
2023-02-16 12:21:02 -0500

    ready

--- src/athlete-card2.js
+++ src/athlete-card2.js
@@ -1,51 +1,117 @@
 import { LitElement, html, css } from 'lit';
 
+
 export class AthleteCard2 extends LitElement {
   
   static get properties() {
     return {
-      version: {
+      name: {
         type: String,
         reflect: true
+      },
+      position: {
+        type: String,
       }
     }
   }
 
   static get styles() {
     return css `
-      :host {
-        font-size: 2em;
+    .card
+    {
+      background-color: grey;
+      border: solid black;
+      width: 400px;
+      display: inline-flex;
+    }
+    
+    .btn { 
+      border: 2px solid blue;
+      text-transform: uppercase;
+      padding: 2px 2px;
+      background-color: white;
+      font-size: 12px;
+      text-align: center;
+      margin: auto;
+      display: flex;
+      display: center;
+    }
+    
+    .title-header
+    {
+      text-align: center;
+    }
+    
+    .desc
+    {
+      text-align: center;
+      display: none;
+    }
+    
+    
+    @media screen and (min-width: 500px) and (max-width: 800px)
+    {
+      .btn
+      {
+        display: none;
       }
+    }
+    
+    @media screen and (max-width: 500px)
+    {
+      
+      .card
+      {
+        scale: 0.7;
+      }
+      
+      .img
+      {
+        scale: 0.6;
+      }
+     
+    }
+    
+    .img {
+      width: 400px;
+    }
+    
+    .duplicate:hover {
+      background-color: yellow;
+    }
+    
+    .duplicate:focus {
+      background-color: yellow;
+    }
+    
+    
+    
+    
     `;
   }
 
   constructor() {
     super();
-    this.version = 'STARTING';
+    this.name = 'Jalen Hurts';
+    this.position = 'Quarterback';
   }
 
   render() {
     return html`
-    <button class="duplicate"> Duplicate card </button>
-
-<button class="newcolor"> Toggle background color </button>
-  
-<button class="something"> Something else </button>
-  
-<button class="delete"> Delete last card </button>
-
-<div class="card" id="card1">
-  <div>
-  <div class="title-header">
-    <h2> Jalen Hurts </h2>
-  </div>
-  <img class = "img" src="https://th.bing.com/th/id/OIP.NX-MOMDx5uAkx4YnlPWexAHaE7?pid=ImgDet&rs=1" alt="jalen hurts">
-  <p class = "desc"> Jalen Hurts is a football player </p>
-  <!--<a href="https://hax.psu.edu/">-->
-  <button class="btn" id='details'> Details </button>
-  </a>
-</div>
-</div>
+   
+      <div class="card" id="card1">
+        <div>
+        <div class="title-header">
+          <h2> $(this.name) </h2>
+          <h3> $(this.position) </h3>
+        </div>
+        <img class = "img" src="https://th.bing.com/th/id/OIP.NX-MOMDx5uAkx4YnlPWexAHaE7?pid=ImgDet&rs=1" alt="jalen hurts">
+        <p class = "desc"> Jalen Hurts is a football player </p>
+        <!--<a href="https://hax.psu.edu/">-->
+        <button class="btn" id='details'> Details </button>
+        </a>
+      </div>
+      </div>
     `;
   }
 }
